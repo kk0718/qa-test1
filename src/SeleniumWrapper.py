941a2e0781a291e5a60764d1de842aade9461a48
kk0718 <keyurtrivedi711@gmail.com>
2018-10-07 21:48:34 -0400

    Update src/SeleniumWrapper.py

---
+++ src/SeleniumWrapper.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+###############################################
+#
+# This Library provides the Keyword Enhancement
+# for the robot-framework Selenium Library.
+#
+################################################
+
+from Selenium2Library import Selenium2Library
+
+class SeleniumWrapper(Selenium2Library):
+    pass
